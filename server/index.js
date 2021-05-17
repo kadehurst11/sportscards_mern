90992b385e4c2d47e594d25a0a4aed2ab5d41cbc
Kade Hurst <kadehurst@Kades-MacBook-Pro.local>
2021-05-16 19:37:47 -0600

    App using mongodb as backend database is able to create delete edit and like posts need to add user authentication

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const App = express();
+import postRoutes from './routes/posts.js';
 
-App.use(bodyParser.json({limit: "30mb", extended: true}));
-App.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-App.use(cors());
+const app = express();
 
-const CONNECTION_URL = 'mongodb+srv://kadeh:kadeh123@cluster0.c75jf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => App.listen(PORT, () => console.log('server running on port: ${PORT}')))
-    .catch((error) => console.log(error.message));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-mongoose.set('useFindAndModify' , false);
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://kadeh:kadeh123@cluster0.c75jf.mongodb.net/posts?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
